97fd39c431a88b0a7e8f36c379c3efce189acdfd
anozeref <kantorposfikry@gmail.com>
2025-09-19 15:02:57 +0700

    Tambah Halaman Produk

--- test-ejs/app.js
+++ test-ejs/app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
@@ -6,7 +7,7 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.use(express.static('layout'));
 function renderWithLayout(res, view, options) {
   ejs.renderFile(path.join(__dirname, 'views', view + '.ejs'), options, (err, str) => {
     if (err) throw err;
@@ -34,6 +35,20 @@ app.get('/contact', (req, res) => {
     heading: 'Hubungi Kami'
   });
 });
+app.get('/product', (req, res) => {
+  const daftarProduk = [
+    { id: 1, nama: 'Laptop', harga: 7500000, img: "https://images.tokopedia.net/img/JFrBQq/2024/1/16/4b9cc476-f5fc-45ee-a469-8b077a51d5e7.jpg" },
+    { id: 2, nama: 'Smartphone', harga: 3500000, img:"https://www.ufoelektronika.com/image/cache/catalog/A54/A54-1000x1000.jpg" },
+    { id: 3, nama: 'Headset', harga: 250000, img:"https://png.pngtree.com/png-clipart/20230416/ourmid/pngtree-headset-gaming-headset-png-image_6706672.png" },
+    { id: 4, nama: 'Keyboard', harga: 150000, img: "https://www.nicepng.com/png/detail/21-212212_gaming-keyboard-png-svg-library-library-controls-for.png" }
+  ];
+
+  renderWithLayout(res, 'product', {
+    title: 'Daftar Produk',
+    heading: 'Produk Kami',
+    products: daftarProduk
+  });
+});
 
 app.get('/product/:id/category/:idCat', (req, res) => {
   res.send(
@@ -41,7 +56,7 @@ app.get('/product/:id/category/:idCat', (req, res) => {
   );
 });
 
-// halaman 404
+// halaman Error
 app.use((req, res) => {
   res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
